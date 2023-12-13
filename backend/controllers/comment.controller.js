f0c63e252c258f10f2b8c049fc4efa51e068ca53
fikiaprian23 <fikiaprian23@gmail.com>
2023-12-13 12:49:44 +0700

    add CRUD comment api

--- backend/controllers/comment.controller.js
+++ backend/controllers/comment.controller.js
@@ -1,65 +1,119 @@
-import Comment from "../models/comment.model.js";
+import Comment from '../models/comment.model.js'
+import Post from '../models/post.model.js'
 
 const commentController = {
-  getComments: async (req, res) => {
+  commentToPost: async (req, res) => {
     try {
-      const comments = await Comment.find();
-      res.json({ message: "Get All Comments Success", comments });
-    } catch (error) {
+      const { textComment } = req.body
+      const postId = req.params.postId
+      const commentedBy = req.user._id
+
+      console.log(postId)
+
+      if (!textComment) {
+        return res.status(400).json({ error: 'Text field is required' })
+      }
+
+      const post = await Post.findById(postId)
+      if (!post) {
+        return res.status(404).json({ error: 'Post not found' })
+      }
+
+      const newComment = {
+        postId,
+        textComment,
+        commentedBy,
+      }
+
+      const savedComment = await Comment.create(newComment)
+
+      res.status(200).json({ message: 'Comment on Post Success', savedComment })
+    } catch (err) {
       res
         .status(500)
-        .json({ message: "Failed Get All Comments", detail: error.message });
+        .json({ message: 'Failed Create Comment', detail: err.message })
     }
   },
-  getCommentById: async (req, res) => {
+
+  getCommentsByPost: async (req, res) => {
     try {
-      const comment = await Comment.findById(req.params.id);
-      res.json({ message: "Get Comment Success", comment });
-    } catch (error) {
+      const postId = req.params.postId
+
+      const post = await Post.findById(postId)
+      if (!post) {
+        return res.status(404).json({ error: 'Post not found' })
+      }
+
+      const comments = await Comment.find({ postId })
+
+      res.status(200).json({ comments })
+    } catch (err) {
       res
-        .status(400)
-        .json({ message: "Failed Get Comment", detail: error.message });
+        .status(500)
+        .json({ message: 'Failed to retrieve comments', detail: err.message })
     }
   },
-  createComment: async (req, res) => {
-    const comment = new Comment(req.body);
-    try {
-      const savedComment = await comment.save();
-      res.status(201).json({ message: "Create Comment Success", savedComment });
-    } catch (error) {
-      res
-        .status(400)
-        .json({ message: "Failed Create Comment", detail: error.message });
-    }
-  },
+
   updateComment: async (req, res) => {
     try {
-      const updatedComment = await Comment.findByIdAndUpdate(
-        req.params.id,
-        req.body,
-        { new: true }
-      );
+      const { textComment } = req.body
+      const commentId = req.params.commentId
+      const commentedBy = req.user._id
+
+      if (!textComment) {
+        return res.status(400).json({ error: 'Text field is required' })
+      }
+
+      const comment = await Comment.findById(commentId)
+      if (!comment) {
+        return res.status(404).json({ error: 'Comment not found' })
+      }
+
+      // Check if the user is authorized to update the comment
+      if (comment.commentedBy.toString() !== commentedBy.toString()) {
+        return res
+          .status(403)
+          .json({ error: 'Unauthorized to update this comment' })
+      }
+
+      comment.textComment = textComment
+      const updatedComment = await comment.save()
+
       res
         .status(200)
-        .json({ message: "Update Comment Success", updatedComment });
-    } catch (error) {
+        .json({ message: 'Comment updated successfully', updatedComment })
+    } catch (err) {
       res
-        .status(400)
-        .json({ message: "Failed Update Comment", detail: error.message });
+        .status(500)
+        .json({ message: 'Failed to update comment', detail: err.message })
     }
   },
+
   deleteComment: async (req, res) => {
     try {
-      const deletedComment = await Comment.findByIdAndDelete(req.params.id);
+      const commentId = req.params.commentId
+
+      const comment = await Comment.findById(commentId)
+      if (!comment) {
+        return res.status(404).json({ error: 'Comment not found' })
+      }
+
+      // Check if the user is authorized to delete the comment
+      if (comment.commentedBy.toString() !== req.user._id.toString()) {
+        return res
+          .status(403)
+          .json({ error: 'Unauthorized to delete this comment' })
+      }
+
+      await Comment.findByIdAndDelete(commentId)
+
+      res.status(200).json({ message: 'Comment deleted successfully' })
+    } catch (err) {
       res
-        .status(200)
-        .json({ message: "Delete Comment Success", deletedComment });
-    } catch (error) {
-      res
-        .status(400)
-        .json({ message: "Failed Delete Comment", detail: error.message });
+        .status(500)
+        .json({ message: 'Failed to delete comment', detail: err.message })
     }
   },
-};
+}
 
-export default commentController;
+export default commentController
--- backend/routes/comment.routes.js
+++ backend/routes/comment.routes.js
@@ -1,139 +1,19 @@
-import express from "express";
-import commentController from "../controllers/comment.controller.js";
+import express from 'express'
+import commentController from '../controllers/comment.controller.js'
+import protectRoute from '../middleware/authorization.js'
 
-const router = express.Router();
+const router = express.Router()
 
-/**
- * @swagger
- * tags:
- *   name: Comments
- *   description: API for managing comments.
- */
+// Create comment to post
+router.post('/:postId', protectRoute, commentController.commentToPost)
 
-/**
- * @swagger
- * /comments:
- *   get:
- *     summary: Get all comments
- *     tags: [Comments]
- *     responses:
- *       '200':
- *         description: Successful response
- *         content:
- *           application/json:
- *             example: { message: "Get All Comments Success", comments: [] }
- */
-router.get("/", commentController.getComments);
+// Get comments by post
+router.get('/:postId', protectRoute, commentController.getCommentsByPost)
 
-/**
- * @swagger
- * /comments/{id}:
- *   get:
- *     summary: Get a comment by ID
- *     tags: [Comments]
- *     parameters:
- *       - in: path
- *         name: id
- *         required: true
- *         description: ID of the comment
- *         schema:
- *           type: string
- *     responses:
- *       '200':
- *         description: Successful response
- *         content:
- *           application/json:
- *             example: { message: "Get Comment Success", comment: {} }
- *       '400':
- *         description: Failed response
- *         content:
- *           application/json:
- *             example: { message: "Failed Get Comment", detail: "Error message" }
- */
-router.get("/:id", commentController.getCommentById);
+// Update a comment
+router.put('/:commentId', protectRoute, commentController.updateComment)
 
-/**
- * @swagger
- * /comments/create:
- *   post:
- *     summary: Create a new comment
- *     tags: [Comments]
- *     requestBody:
- *       required: true
- *       content:
- *         application/json:
- *           example: { comment: "This is a new comment", postId: "postId" }
- *     responses:
- *       '201':
- *         description: Successful response
- *         content:
- *           application/json:
- *             example: { message: "Create Comment Success", savedComment: {} }
- *       '400':
- *         description: Failed response
- *         content:
- *           application/json:
- *             example: { message: "Failed Create Comment", detail: "Error message" }
- */
-router.post("/create", commentController.createComment);
+// Delete a comment
+router.delete('/:commentId', protectRoute, commentController.deleteComment)
 
-/**
- * @swagger
- * /comments/{id}:
- *   patch:
- *     summary: Update a comment by ID
- *     tags: [Comments]
- *     parameters:
- *       - in: path
- *         name: id
- *         required: true
- *         description: ID of the comment
- *         schema:
- *           type: string
- *     requestBody:
- *       required: true
- *       content:
- *         application/json:
- *           example: { comment: "Updated comment" }
- *     responses:
- *       '200':
- *         description: Successful response
- *         content:
- *           application/json:
- *             example: { message: "Update Comment Success", updatedComment: {} }
- *       '400':
- *         description: Failed response
- *         content:
- *           application/json:
- *             example: { message: "Failed Update Comment", detail: "Error message" }
- */
-router.patch("/:id", commentController.updateComment);
-
-/**
- * @swagger
- * /comments/{id}:
- *   delete:
- *     summary: Delete a comment by ID
- *     tags: [Comments]
- *     parameters:
- *       - in: path
- *         name: id
- *         required: true
- *         description: ID of the comment
- *         schema:
- *           type: string
- *     responses:
- *       '200':
- *         description: Successful response
- *         content:
- *           application/json:
- *             example: { message: "Delete Comment Success", deletedComment: {} }
- *       '400':
- *         description: Failed response
- *         content:
- *           application/json:
- *             example: { message: "Failed Delete Comment", detail: "Error message" }
- */
-router.delete("/:id", commentController.deleteComment);
-
-export default router;
+export default router
