8bea6f6463ab6a03ebfd795ccbd1a6a647fc8c25
fikiaprian23 <fikiaprian23@gmail.com>
2023-12-17 09:20:14 +0700

    fix edit profile

--- frontend/src/components/Settings/EditProfile.jsx
+++ frontend/src/components/Settings/EditProfile.jsx
@@ -200,7 +200,7 @@ export const EditProfile = ({ user }) => {
                   onClick={handleEditProfile}
                   className="text-white bg-indigo-700  hover:bg-indigo-800 focus:ring-4 focus:outline-none focus:ring-indigo-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-indigo-600 dark:hover:bg-indigo-700 dark:focus:ring-indigo-800"
                 >
-                  Save
+                  {loading ? 'Loading...' : 'Save'}
                 </button>
               </div>
             </div>
